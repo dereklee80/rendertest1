50eba9d6a12721688ea21973d4a2a4c8df14747c
dereklee80 <derek_lee@rp.edu.sg>
2025-12-09 11:02:18 +0800

    3rd commit

--- db.js
+++ db.js
@@ -8,7 +8,7 @@
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         waitForConnections: true,
-        connectionLimit: 10,
+        connectionLimit: 100,
         queueLimit: 0
     });
 
