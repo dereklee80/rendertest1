3b424375ef306018f23043580f72e4d7fdad5bed
dereklee80 <derek_lee@rp.edu.sg>
2025-12-15 00:02:25 +0800

    25th commit

--- server.js
+++ server.js
@@ -1,50 +1,58 @@
 // server.js
 const express = require('express');
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 //const fs = require('fs');
 const port = 3000;
 
 
 const dbConfig = {
-    host: "mysql-2f879c75-sicshot-2c3d.l.aivencloud.com",
-    user: "avnadmin",
-    password: "AVNS_WdWiAYWkKKegwTt47Ja",
-    database: "defaultdb",
-    port: 14495,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
-
 };
+
+/*
+ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync('./ca.pem').toString(), // Path to your downloaded CA certificate
+    }
+ */
 
 const app = express();
 app.use(express.json()); // Parse JSON request bodies
 
 // Example Route: Get all cards
-app.get('/allcards23', async (req, res) => {
+app.get('/allcards25', async (req, res) => {
     try {
         let connection = await mysql.createConnection(dbConfig);
         const [rows] = await connection.execute('SELECT * FROM defaultdb.card_test');
         res.json(rows);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error - allcards23' });
+        res.status(500).json({ message: 'Server error - allcards25' });
     }
 });
 
-/*
+
 // Example Route: Create a new card
-app.post('/addcard', async (req, res) => {
+app.get('/addcard', async (req, res) => {
     const { card_name, card_qty } = req.body;
     try {
-        await db.query('INSERT INTO card_test (card_name, card_qty) VALUES (?, ?)', [card_name, card_qty]);
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO card_test (card_name, card_qty) VALUES (?, ?)', [card_name, card_qty]);
         res.status(201).json({ message: 'Card added successfully' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error - addcard' });
     }
 });
-*/
+
 
 //start the server
 app.listen(port, () => {
